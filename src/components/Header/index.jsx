acb82cadfcb7d4c1fe6ceaaa882dc4c92d221666
Hung Dinh Trong <dinhhung18052000@gmail.com>
2022-07-04 21:56:34 +0700

    change cv url

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,10 @@
-import { ScrollContext } from "App";
-import { Button } from "GlobalStyled";
-import React, { useContext, useRef } from "react";
-import { FaBars, FaMoon, FaSun, FaTimes } from "react-icons/fa";
-import { Link } from "react-scroll";
-import { ThemeContext } from "styled-components";
-import { Logo, Nav, NavBtn, NavList, StyledFaBars, ThemeBtn } from "./StyledHeader";
+import { ScrollContext } from 'App';
+import { Button } from 'GlobalStyled';
+import React, { useContext, useRef } from 'react';
+import { FaBars, FaMoon, FaSun, FaTimes } from 'react-icons/fa';
+import { Link } from 'react-scroll';
+import { ThemeContext } from 'styled-components';
+import { Logo, Nav, NavBtn, NavList, StyledFaBars, ThemeBtn } from './StyledHeader';
 
 const Header = () => {
   const context = useContext(ScrollContext);
@@ -14,43 +14,43 @@ const Header = () => {
   const closeNavList = () => {
     const width = window.innerWidth;
     if (width <= 768) {
-      return (ref.current.style.transform = "translateX(-50vw)");
+      return (ref.current.style.transform = 'translateX(-50vw)');
     } else if (width > 768 && width <= 1024) {
-      return (ref.current.style.transform = "translateX(-30vw)");
+      return (ref.current.style.transform = 'translateX(-30vw)');
     } else return;
   };
   const toggleTheme = () => {
-    if (theme === "light") setTheme("dark");
-    else setTheme("light");
+    if (theme === 'light') setTheme('dark');
+    else setTheme('light');
   };
   const showNavList = () => {
-    ref.current.style.transform = "translateX(0)";
+    ref.current.style.transform = 'translateX(0)';
   };
   return (
     <ScrollContext.Consumer>
       {() => (
         <Nav fixed={context.isScroll}>
-          <div style={{ display: "flex", alignItems: "flex-end" }}>
+          <div style={{ display: 'flex', alignItems: 'flex-end' }}>
             <StyledFaBars onClick={showNavList}>
               <FaBars />
             </StyledFaBars>
             <Logo>
-              <a href="/">Portlifio</a>
+              <a href='/'>Portlifio</a>
             </Logo>
           </div>
           <NavList ref={ref}>
             <li>
-              <FaTimes className="close-icon" onClick={() => closeNavList()} />
+              <FaTimes className='close-icon' onClick={() => closeNavList()} />
             </li>
             <li>
               <Link
                 onClick={() => closeNavList()}
-                className="link"
-                to="about-me"
+                className='link'
+                to='about-me'
                 spy={true}
                 smooth={true}
                 duration={1000}
-                activeClass="active"
+                activeClass='active'
                 offset={-100}
               >
                 About Me
@@ -59,12 +59,12 @@ const Header = () => {
             <li>
               <Link
                 onClick={() => closeNavList()}
-                className="link"
-                to="projects"
+                className='link'
+                to='projects'
                 spy={true}
                 smooth={true}
                 duration={1000}
-                activeClass="active"
+                activeClass='active'
                 offset={-100}
               >
                 Projects
@@ -73,12 +73,12 @@ const Header = () => {
             <li>
               <Link
                 onClick={() => closeNavList()}
-                className="link"
-                to="contact"
+                className='link'
+                to='contact'
                 spy={true}
                 smooth={true}
                 duration={1000}
-                activeClass="active"
+                activeClass='active'
                 offset={-100}
               >
                 Contact
@@ -87,13 +87,11 @@ const Header = () => {
           </NavList>
           <NavBtn>
             <ThemeBtn onClick={toggleTheme}>
-              <FaSun className="sunIcon" />
-              <FaMoon className="moonIcon" />
+              <FaSun className='sunIcon' />
+              <FaMoon className='moonIcon' />
             </ThemeBtn>
             <Button>
-              <a href="https://drive.google.com/file/d/1Iy-fBPfeS7QBf3r115LPCCe13xWEFI4c/view?usp=sharing">
-                Resume
-              </a>
+              <a href='https://drive.google.com/file/d/1XBn0SWDWzvz89fwl7tyl20d_thRHdJvq/view?usp=sharing'>Resume</a>
             </Button>
           </NavBtn>
         </Nav>
